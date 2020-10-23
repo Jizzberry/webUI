5ae9d444f64da6604e928bb37c040fd7141710ec
Ovenoboyo <ovenoboyo@gmail.com>
2020-10-23 19:06:55 +0530

    init plyr submodule

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,1 +1,56 @@
-<template></template>
+<template>
+  <div>
+    <Plyr>
+      <video
+        crossorigin
+        playsinline
+        poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
+        src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+      >
+        <source
+          size="576"
+          src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+          type="video/mp4"
+        />
+        <source
+          size="720"
+          src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4"
+          type="video/mp4"
+        />
+        <source
+          size="1080"
+          src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4"
+          type="video/mp4"
+        />
+        <track
+          default
+          kind="captions"
+          label="English"
+          src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt"
+          srclang="en"
+        />
+        <track
+          kind="captions"
+          label="Français"
+          src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt"
+          srclang="fr"
+        />
+        <a
+          download=""
+          href="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+        >
+          Download
+        </a>
+      </video>
+    </Plyr>
+  </div>
+</template>
+
+<script>
+import Plyr from "@/components/Plyr";
+export default {
+  components: {
+    Plyr,
+  },
+};
+</script>
